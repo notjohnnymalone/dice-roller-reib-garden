fe061cff4adfd93a3617444cd4451681cc5cc4f9
SirRipher <90797166+SirRipher@users.noreply.github.com>
2021-09-17 13:19:14 -0600

    testing

--- Dice_Roller_reib_garden_V1.py
+++ Dice_Roller_reib_garden_V1.py
@@ -2,3 +2,12 @@
 #CP2-F04 Team Programming
 #Reid A. Martin & Graden Rusk
 ##-------------------##
+
+
+
+#----Functions start here----#
+
+import random
+
+def welcome:
+    print("Hello! and welcome to Reib and Gardens Dice Roller!")
