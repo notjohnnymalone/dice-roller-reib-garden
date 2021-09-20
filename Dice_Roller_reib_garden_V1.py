9e5cd7a7efc474686a57ba40e496ca702261285a
notjohnnymalone <90425889+notjohnnymalone@users.noreply.github.com>
2021-09-20 12:57:25 -0600

    Update Dice_Roller_reib_garden_V1.py
    added player select function

--- Dice_Roller_reib_garden_V1.py
+++ Dice_Roller_reib_garden_V1.py
@@ -12,5 +12,18 @@ import random
 def welcome:
     print("Hello! and welcome to Reib and Gardens Dice Roller!")
 
-
-
+#reid edit 1
+def player_select:
+    rorg = 0
+    print("Who do you want to play as?/nGarden or Reib?")
+    player = input(">>> ")
+    if player = ['graden', 'garden', 'gar', 'Graden', 'GRADEN', 'Garden', 'GARDEN','Gar', 'GAR', 'dumbbum']:
+        rorg = 1
+        print("You have chosen Graden.../nIf you didn't.../n/nshut up.")
+    else:
+        rorg = 2
+        print("You have chosen Reid.")
+        time.sleep(3)
+        print("if you didn't...")
+        time.sleep(3)
+        print("i don't care.")
